5a4ae0f47198467611c355fc131d512ad70d463e
devesraamahmoud <esraa.dev@gmail.com>
2024-06-13 15:20:58 +0300

    Fix delete method on post item
    - I fix delete method by binding the post model to the delete method

--- app/Livewire/Posts/PostItem.php
+++ app/Livewire/Posts/PostItem.php
@@ -9,15 +9,15 @@ class PostItem extends Component
 {
     public Post $post;
 
-    public function edit($post){
+    public function edit(Post $post){
         return redirect()->route('posts.edit', $post);
     }
 
-    public function show($post){
+    public function show(Post $post){
         return redirect()->route('posts.show', $post);
     }
 
-    public function delete($post)
+    public function delete(Post $post)
     {
         $post->delete();
         session()->flash('message', 'Post deleted successfully.');
